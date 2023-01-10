7ab609114707880a15d7a8655bf4c73bfbd03166
Theophilus Gordon <theophilus.gordon@amalitech.org>
2023-01-10 17:36:39 +0000

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Quiz App</h1>
+    </div>
+  );
+}
+
+export default App;
